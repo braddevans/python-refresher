b571ae5ae47183406fc633d2127b809725e49507
bradd evans <braddevans123@gmail.com>
2020-03-25 22:09:50 +0000

    initial commit

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+def printlist(list):
+    for i in list:
+        print(i)
+
+
+def dictionarylookup(str, dictionary):
+    print(f"{str} = {dictionary[str]}")
+
+
+def stringprint(args):
+    print(f"{args}")
